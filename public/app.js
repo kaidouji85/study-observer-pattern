5bcc923d428b3eb591abb65a033d8d4a16b14a9b
takeuchi-y <takeuchi.y@opst.co.jp>
2020-03-09 16:48:47 +0900

    Appクラスを追加した

--- public/app.js
+++ public/app.js
@@ -1,6 +1,22 @@
 import {ReverseString} from './reverse-string.js';
 
 window.addEventListener('load', () => {
-  const body = window.document.body || document.createElement('body');
-  const reverseString = new ReverseString(body);
+  const app = new App();
 });
+
+/**
+ *
+ * @class App
+ * @property {ReverseString} _reverseString 文字反転コンポネント
+ */
+class App {
+  /**
+   * コンストラクタ
+   *
+   * @constructor
+   */
+  constructor() {
+    const body = window.document.body || document.createElement('body');
+    this._reverseString = new ReverseString(body);
+  }
+}
